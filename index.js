6244a2f6b961ee12ea9c41b3fbc6458d7a5cc639
Rodney St Cloud <hoyrod1@aol.com>
2024-05-22 22:35:03 -0400

    Made some minor changes to my CSS file for the index.ejs page

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
-console.log()(
+console.log(
   "============================= ALAB-318-2-1-express-application ============================="
 );
-const { response } = require("express");
-const { request } = require("express");
+// const { response } = require("express");
+// const { request } = require("express");
 const express = require("express");
+const indexRouter = require("./routes/index.js");
 const app = express();
 const port = 3030;
+/**
+ * The set up for the views engine to retrieve files from the view directory, defined EJS as the views engine,    
+ 
+ * The set up for the JSON middleware handler
+ 
+ * The set up for the express urlencoded niddleware handler 
+ 
+ * The set up for the extended query string to true to except nested data and set up express.static to    first retrieve static data from the public directory
+ */
+//---------------------------------------------------------------------------------------------//
 app.set("views", "views");
-app.set("view emgine", "ejs");
-//----------------------------//
+app.set("view engine", "ejs");
+//---------------------------------------------------------------------------------------------//
 app.use(express.json());
+//---------------------------------------------------------------------------------------------//
 app.use(express.urlencoded({ extended: true }));
+//---------------------------------------------------------------------------------------------//
 app.use(express.static("public"));
+//---------------------------------------------------------------------------------------------//
 app.use("/", indexRouter);
-
+//---------------------------------------------------------------------------------------------//
 app.listen(port, () => {
   console.log(`The server is listening on port: ${port}`);
 });
-
+//---------------------------------------------------------------------------------------------//
 // app.get("/", (request, response) => {
 //   response.send("Welcome To My First Express Application");
 // });
